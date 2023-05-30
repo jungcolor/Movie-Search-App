add0af5011b64038aa985299d78ca32a265c8499
jungcolor <jungcolor@gmail.com>
2023-05-31 00:32:30 +0900

    Feat - tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 const App = () => {
-    return <div>하이 리액트 (feat: typescript)</div>
+    return (
+        <>
+            <div className="font-bold">영화 검색 사이트를 만들어보자!!</div>
+            <p className="text-2xl">기능도 붙여가면서~~</p>
+        </>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import './styles/tailwind.css';
+
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
 
