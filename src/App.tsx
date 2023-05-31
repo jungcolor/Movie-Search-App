89fc2412cdef3a5ed4b1207641261a9aac1b9e97
jungcolor <jungcolor@gmail.com>
2023-05-31 23:49:41 +0900

    Feat - Layout Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 const App = () => {
     return (
         <>
-            <div className="font-bold">영화 검색 사이트를 만들어보자!!</div>
-            <p className="text-2xl">기능도 붙여가면서~~</p>
+            <header>
+                <h1>Header</h1>
+                <div>
+                    <h2>로고</h2>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">목록1</a></li>
+                        <li><a href="#">목록2</a></li>
+                        <li><a href="#">목록3</a></li>
+                        <li><a href="#">목록4</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <div id="container">
+                <h1>Container</h1>
+                <div id="serach">
+                    <h2>검색</h2>
+                </div>
+                <div id="content">
+                    {/* 동적으로 변경 될 영역 */}
+                    <h2>내용</h2>
+                </div>
+            </div>
         </>
     )
 }
