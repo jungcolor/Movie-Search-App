4a0e8b82571010389be5582c95ccb8aec6cad7fc
jungcolor <jungcolor@gmail.com>
2023-06-02 00:11:58 +0900

    Feat - Design Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 const App = () => {
     return (
         <div id="wrpper" className="min-h-screen flex flex-col h-screen">
-            <header className="flex flex-col items-center justify-center z-50 w-full text-sm py-4">
+            <header className="flex flex-col items-center justify-center z-50 w-full py-4 border-b-2 border-b-teal-500 bg-white">
                 <h1 className="mb-6">
-                    <a href="#" className="text-3xl font-semibold">영화 검색 사이트</a>
+                    <a href="#" className="text-3xl font-semibold text-teal-500">영화 검색 사이트</a>
                 </h1>
                 <nav className="flex">
-                    <a href="#" className="mx-4">목록1</a>
-                    <a href="#" className="mx-4">목록2</a>
-                    <a href="#" className="mx-4">목록3</a>
-                    <a href="#" className="mx-4">목록4</a>
-                    <a href="#" className="mx-4">목록5</a>
+                    <a href="#" className="mx-6 text-teal-800 hover:text-teal-800">목록1</a>
+                    <a href="#" className="mx-6 text-teal-800 hover:text-teal-800">목록2</a>
+                    <a href="#" className="mx-6 text-teal-800 hover:text-teal-800">목록3</a>
+                    <a href="#" className="mx-6 text-teal-800 hover:text-teal-800">목록4</a>
+                    <a href="#" className="mx-6 text-teal-800 hover:text-teal-800">목록5</a>
                 </nav>
             </header >
             <main id="container" className="w-full h-screen py-8 px-8">
-                <article id="serach" className="flex justify-center">
+                <article id="serach" className="flex justify-center mb-8">
                     <input type="text" className="w-80 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="검색어를 입력하세요" />
                     <button type="button" className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">검색버튼</button>
                 </article>
@@ -23,7 +23,7 @@ const App = () => {
                     <h2>내용</h2>
                 </div>
             </main>
-        </div >
+        </div>
     )
 }
 
