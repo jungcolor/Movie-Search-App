ab3b276bc94b722e9163b9927fddb5df8a6d0a08
jungcolor <jungcolor@gmail.com>
2023-06-01 23:34:37 +0900

    Feat - Basic Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 const App = () => {
     return (
-        <>
-            <header>
-                <h1>Header</h1>
-                <div>
-                    <h2>로고</h2>
-                </div>
-                <nav>
-                    <ul>
-                        <li><a href="#">목록1</a></li>
-                        <li><a href="#">목록2</a></li>
-                        <li><a href="#">목록3</a></li>
-                        <li><a href="#">목록4</a></li>
-                    </ul>
+        <div id="wrpper" className="min-h-screen flex flex-col h-screen">
+            <header className="flex flex-col items-center justify-center z-50 w-full text-sm py-4">
+                <h1 className="mb-6">
+                    <a href="#" className="text-3xl font-semibold">로고</a>
+                </h1>
+                <nav className="flex">
+                    <a href="#" className="mx-4">목록1</a>
+                    <a href="#" className="mx-4">목록2</a>
+                    <a href="#" className="mx-4">목록3</a>
+                    <a href="#" className="mx-4">목록4</a>
+                    <a href="#" className="mx-4">목록5</a>
                 </nav>
-            </header>
-            <div id="container">
-                <h1>Container</h1>
+            </header >
+            <main id="container" className="w-full h-screen py-10 px-10">
                 <div id="serach">
                     <h2>검색</h2>
                 </div>
@@ -24,8 +21,8 @@ const App = () => {
                     {/* 동적으로 변경 될 영역 */}
                     <h2>내용</h2>
                 </div>
-            </div>
-        </>
+            </main>
+        </div >
     )
 }
 
