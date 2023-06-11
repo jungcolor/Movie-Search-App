c928540416181029661b326c39fe209c7a5e5ade
jungcolor <jungcolor@gmail.com>
2023-06-11 22:40:05 +0900

    Refactor - Add of Search Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Navigation from "@components/navigation/Navigation";
 import Card from "@components/card/Card";
+import Search from "@components/search/Serach";
 
-export { Navigation, Card };
+export { Navigation, Card, Search };
