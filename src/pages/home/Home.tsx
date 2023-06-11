d7e12b680eea35f18552fcdc2d4deba560ae5c90
jungcolor <jungcolor@gmail.com>
2023-06-11 14:33:45 +0900

    Feat - 영화 디테일 정보 추가 (poster, overview, date등)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,6 +7,9 @@ const Home = () => {
     const BASE_URL = `https://api.themoviedb.org/3`;
     const API_KEY = `api_key=b5810e430a249494e0cfef5beddec241`;
     const API_URL = BASE_URL + `/movie/now_playing?${API_KEY}&language=ko&page=1`;
+
+    const BASE_IMG_URL = "https://image.tmdb.org/t/p/w500";
+
     const fetchDatas = async () => {
         const datas = await axios.get(API_URL).then(response => {
             if (response.status === 200) {
@@ -28,8 +31,13 @@ const Home = () => {
     return (
         loading ? <h1>로딩중..</h1> : videos?.map(video => {
             return (
-                <div>
+                <div className="mb-6" key={video.id}>
+                    <div>
+                        <img src={BASE_IMG_URL + video.poster_path} />
+                    </div>
                     <div>{video.title}</div>
+                    <div>{video.overview}</div>
+                    <div>{video.release_date}</div>
                 </div>
             )
         })
