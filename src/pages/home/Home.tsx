3f97dc10f9c804a03c9602ed556281291760979f
jungcolor <jungcolor@gmail.com>
2023-06-11 14:25:18 +0900

    Feat - Video 목록 가져오기 with TMDB

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
+    const [videos, setVideos] = useState([]);
+    const [loading, setLoading] = useState(true);
     const BASE_URL = `https://api.themoviedb.org/3`;
     const API_KEY = `api_key=b5810e430a249494e0cfef5beddec241`;
     const API_URL = BASE_URL + `/movie/now_playing?${API_KEY}&language=ko&page=1`;
-    const aa = null;
     const fetchDatas = async () => {
         const datas = await axios.get(API_URL).then(response => {
             if (response.status === 200) {
@@ -13,8 +14,8 @@ const Home = () => {
             }
         });
 
-        console.log(datas);
-
+        setVideos(datas);
+        setLoading(false);
         return datas;
     };
 
@@ -22,8 +23,16 @@ const Home = () => {
         fetchDatas();
     }, []);
 
+    console.log(videos);
+
     return (
-        <h1>메인페이지</h1>
+        loading ? <h1>로딩중..</h1> : videos?.map(video => {
+            return (
+                <div>
+                    <div>{video.title}</div>
+                </div>
+            )
+        })
     )
 }
 
