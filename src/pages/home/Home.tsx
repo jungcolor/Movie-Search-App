9671482d6435a932f8410bc40a0ab1866b0a69a7
jungcolor <jungcolor@gmail.com>
2023-06-20 19:40:12 +0900

    Refactor - Loading 로직 위치 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,12 +10,10 @@ const Home = () => {
 
     const API_KEY = `api_key=b5810e430a249494e0cfef5beddec241`;
     const API_URL = `https://api.themoviedb.org/3/movie/popular?${API_KEY}&language=ko-KR&page=1`;
-
-    // 검색정보로 가져오는 API URL
-    // https://api.themoviedb.org/3/search/movie?${API_KEY}&language=ko-KR&page=1&query=${searchValue}
+    const API_URL_SEARCH = `https://api.themoviedb.org/3/search/movie?${API_KEY}&language=ko-KR&page=1&query=태극기`;
 
     const fetchDatas = async () => {
-        const datas = await axios.get(API_URL).then(response => {
+        const datas = await axios.get(API_URL_SEARCH).then(response => {
             if (response.status === 200) {
                 return response.data.results;
             }
@@ -29,16 +27,21 @@ const Home = () => {
         fetchDatas();
     }, []);
 
+    // 로딩중..
+    if (loading) {
+        return <Spinner />;
+    }
+
     return (
-        loading
-            ?
-            <Spinner />
-            :
-            <div className="flex flex-wrap justify-center">
-                {videos?.map((video: ICard) => {
+        <div className="flex flex-wrap justify-center">
+            {videos.length > 0 ?
+                videos.map((video: ICard) => {
                     return <Card key={video.id} title={video.title} overview={video.overview} poster_path={`https://image.tmdb.org/t/p/w500/${video.poster_path}`}></Card>
-                })}
-            </div >
+                })
+                :
+                <h2>검색결과가 없습니다.</h2> // 컴포넌트로 변경??
+            }
+        </div>
     )
 }
 
