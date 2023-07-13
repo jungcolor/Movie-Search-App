a13671d34bb6baf916c86da2abeffe9bd4a38fa7
reon <reon@cookat.co>
2023-07-13 14:58:15 +0900

    Feat - Card overlay 기능 추가

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,25 +1,21 @@
 import { ICard } from 'types';
 
+// TODO - CSS관련 정리 필요
+import './card.scss';
+
 const Card = (props: ICard) => {
     const { title, poster_path, overview } = props;
 
     return (
-        <div className="relative max-w-xs shadow m-2 cursor-pointer bg-white border border-gray-300 rounded-lg hover:border-teal-600">
-            <div className="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-70 hidden">+</div>
-            <div className="flex min-h-max">
-                <img className="rounded-t-lg" src={poster_path} alt={title} style={{ minHeight: '480px' }} />
+        <div className="card m- relative max-w-xs shadow m-3 box-border cursor-pointer bg-white border-2 border-gray-300 hover:border-teal-500">
+            <div className="card-thumnail relative flex min-h-max">
+                <img src={poster_path} alt={title} style={{ minHeight: '480px' }} />
             </div>
             <div className="p-3">
                 <div>
                     <h5 className="mb-3 text-xl font-bold text-teal-600 tracking-tight">{title}</h5>
                 </div>
                 <p className="text-sm mb-3 font-normal line-clamp-2">{overview !== '' ? overview : '내용없음'}</p>
-                {/* <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-500 rounded-lg hover:bg-teal-800 focus:outline-none ">
-                    더보기
-                    <svg aria-hidden="true" className="w-2 h-2 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
-                    </svg>
-                </a> */}
             </div>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+// TODO - CSS관련 정리 필요
 import './styles/tailwind.css';
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container as Element);
 
 root.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>
+    </BrowserRouter>,
 );
