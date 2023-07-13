d0ddb527c527f50ae6566349944ad15806c7f847
reon <reon@cookat.co>
2023-07-13 18:09:02 +0900

    Feat - Modal Popup 추가 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import { Header, Main } from '@layouts';
 const App = () => {
     return (
         <div id="app" className="min-h-screen flex flex-col h-screen">
+            <div className="modal hidden">
+                <div className="modal-contents relative z-50">
+                    <div className="modal-header text-white">
+                        <h3>TITLE</h3>
+                        <div className="modal-option"></div>
+                    </div>
+                    <div className="modal-body text-white">CONTENTS</div>
+                </div>
+            </div>
+
             <Header />
             <Main />
         </div>
-    )
-}
+    );
+};
 
 export default App;
